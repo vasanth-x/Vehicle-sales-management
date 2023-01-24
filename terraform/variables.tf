d5f79f5b9411e457715dd6519c709c83ced8a016
Vasanth <90051837+vasanth-x@users.noreply.github.com>
2023-01-24 12:19:17 +0530

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+# variable "api-key-source" {
+#   default = "HEADER"
+#   type = string
+#   description = " API key of incoming request for validating "
+# }
+variable "access_key" {
+    description = "access key"
+    default = "AKIA27GN4WVVOZBAPQWQ"
+}
+variable "secret_key" {
+    description = "secret key"
+    default = "NwgPRIBqzRJmBbVsEmshVgR8pH1H2cgbVz68N2n+"
+}
+variable "region" {
+    description = "region"
+    default = "ap-southeast-1" 
+}
